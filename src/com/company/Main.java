8ac583c922bc3c1d923d8a028152914624499b5e
Patience Cellich <pattlartey@yahoo.com>
2021-11-03 12:14:02 -0400

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Battleship");
+        System.out.println("Welcome to the Battleship Game");
 
         new Game();
     }
